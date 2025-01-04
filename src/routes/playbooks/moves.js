eefae9e2c8289d9d3fe257e9541adae976e531e6
Moritz Halke <moritz.halke@divis.io>
2025-01-04 18:16:21 +0100

    add Circle moves

--- src/routes/playbooks/moves.js
+++ src/routes/playbooks/moves.js
@@ -140,14 +140,26 @@ export const moves = [
             'Tell the group and note them down for next session.'
         ,
     }, {
-        name: 'Hit the streets',
-        type: 'circle',
+        name: 'Put a face to a name',
+        type: 'Circle',
+        source: { book: 'Urban Shadows', edition: 2, page: 86 },
         text:
-            '<p>When you hit the streets to get what you need, name who you’re going to and roll with their Circle. On a hit, they’re available and have the stuff! On a 7-9, choose 1:</p>' +
-            '<ul>' +
-            '<li>whoever you’re going to is juggling their own problems</li>' +
-            '<li>whatever you need is more costly than anticipated</li>' +
-            '</ul>',
+            'When you _**put a name to a face or vice versa**_, roll with their Circle.\n\n' +
+            'On a hit, you know their reputation; the MC will tell you what most peaple know about them.\n\n' +
+            'on a 10+, you have dealt with them before; learn something interesting and useful about them or they owe you a Dept, your choice.\n\n' +
+            'On a mill, you do not know them or you owe them, MC\'s choice.'
+        ,
+    }, {
+        name: 'Hit the streets',
+        type: 'Circle',
+        source: {book: 'Urban Shadows', edition: 2, page: 87},
+        text:
+            'When you _**hit the streets to get what you need**_, name who you\'re going to and roll with their Circle.\n\n' +
+            'On a hit, they\'re available and have the stuff!\n\n' +
+            'On a 7-9, choose 1:\n\n' +
+            '- whoever you\'re going to is juggling their own problems\n' +
+            '- whatever you need is more costly than anticipated'
+        ,
         lists: {
             result: '7-9',
             options: [
@@ -155,11 +167,59 @@ export const moves = [
                 'whatever you need is more costly than anticipated',
             ],
         },
-    },
-    {
+    }, {
+        name: 'Study a place of power',
+        type: 'Circle',
+        source: { book: 'Urban Shadows', edition: 2, page: 88 },
+        text:
+            'When you _**study a sanctuary, gathering spot, or place of power**_, roll with the Circle that controls it.\n\n' +
+            'On a hit, you see below the surface to the reality underneath; the MC will reveal an area, NPC, or item located within that is not what it seems.\n\n' +
+            'On a 10+, your insight reveals much about the Circle\'s politics and schemes; ask the MC a relevant quesstion about the Circle and take +1 forward whtn acting on the answer.'
+        ,
+    }, {
+        name: 'Do someone a favor',
+        type: 'Dept',
+        source: { book: 'Urban Shadows', edition: 2, page: 90 },
+        text:
+            'When you _**do someone a favor**_, sey owe you a Dept.'
+        ,
+    }, {
+        name: 'Overlook a harm',
+        type: 'Dept',
+        source: { book: 'Urban Shadows', edition: 2, page: 91 },
+        text:
+            'When someone _**agrees to overlook a harm you\'ve done to them**_, you owe them a Dept.'
+        ,
+    }, {
+        name: 'Cash in a Dept',
+        type: 'Dept',
+        source: { book: 'Urban Shadows', edition: 2, page: 92 },
+        text:
+            'When you _**cash in a Dept**_, remind your debtor why they owe you in order to...\n\n' +
+            '... make a PC:\n\n' +
+            '- answer a question honestly\n' +
+            '- do you a favor at moderate cost\n' +
+            '- _**lend a hand**_ to your efforts\n' +
+            '- _**get in the way**_ of someone else\n' +
+            '- erase a Dept they hold on someone\n\n' +
+            '... make an NPC:\n\n' +
+            '- answer a question honestly\n' +
+            '- arrange a meeting with an NPC in their Circle\n' +
+            '- grant you a worthy boon or useful gift\n' +
+            '- erase a Dept they hold on someone\n' +
+            '- give you a Dept they hold on someone else'
+        ,
+    }, {
         name: 'Refuse to honor a Dept',
         type: 'Dept',
-        source: { book: 'Urban Shadows 2ed', page: 95 },
+        source: { book: 'Urban Shadows', edition: 2, page: 95 },
+        text:
+            'When you _**refuse to honor a dept**_, roll with the difference in Status between you and your creditor.\n\n' +
+            'On a hit, you weasel out of the obligation for now, but you still owe the Dept.\n\”' +
+            'On a 7-9, you iwe them an additional Dept or mark corruption, your choice.\n\n' +
+            'On a miss, you can\'t avoid the hammer: ' +
+            'either honor the dept in full or ' +
+            'erase all the Depts owed to you by their Circle and take -1 ongoing to Status with their Circle until after time has passed.',
         trigger: 'When you _**refuse to honor a Debt**_',
         then: 'roll with the difference in Status between you and your creditor.',
         components: [
@@ -188,5 +248,15 @@ export const moves = [
                 'erase all the Debts owed to you by their Circle and take a -1 ongoing to Status with their Circle until after time passes',
             ],
         ],
+    }, {
+        name: 'Weaken someone\'s standing',
+        type: 'City',
+        source: { book: 'Urban Shadows', edition: 2, page: 97 },
+        text:
+            'When you try to _**weaken someone\'s standing in a Circle with rumor and gossip**_, roll with Status in their circle.\n\m' +
+            'On a hit, the rumors take hold; they take -1 ongoing to moves during the faction turn until they find a way to dispel the whispers.\n\n' +
+            'On a 10+, one of their enemies catches wind of your gossip and approaches you with damaging information about your target or their interests.\n\n' +
+            'On a miss, your target traces the stories directly back to you... and catches you in a vulnerable spot before you can react.'
+        ,
     },
 ];
